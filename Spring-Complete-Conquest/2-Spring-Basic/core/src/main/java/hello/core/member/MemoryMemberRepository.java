01f2340f1e5c437a17a2503e27e040b4b4aa5423
Yesol <ilosey.p@gmail.com>
2023-11-29 22:52:06 +0900

    2023-11-29 스프링 기본 강의 구현 예제

---
+++ Spring-Complete-Conquest/2-Spring-Basic/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    // Hashmap보다 ConcurrentHashMap을 써야 하는데 -> 동시성때문에
+    // 예제니까~ 그냥 HashMap 씀.
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
