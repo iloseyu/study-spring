60c7df110dfc326d7d16f07f080b6d68aa82a087
Yesol <ilosey.p@gmail.com>
2023-12-27 23:11:58 +0900

    2023-12-27 리퀘스트 스코프 예제 만들기 ~ 빈 스코프 완료

---
+++ Spring-Complete-Conquest/2-Spring-Basic/core/src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,37 @@
+package hello.core.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class MyLogger {
+    // 동시에 여러 요청이 들어온대도, 각각의 요청을 처리
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + " [" + requestURL + "] " + message);
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "]" + " request scope bean create: " + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "]" + " request scope bean close: " + this);
+    }
+
+}
