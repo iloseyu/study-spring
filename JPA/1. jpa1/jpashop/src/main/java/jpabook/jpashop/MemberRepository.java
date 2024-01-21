55d7c11860b462665f76bbbc3a0d3eb50e4ac58c
Yesol <ilosey.p@gmail.com>
2024-01-21 23:19:16 +0900

    2024-01-21 ~ 프로젝트 환경설정

---
+++ JPA/1. jpa1/jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+
+}
